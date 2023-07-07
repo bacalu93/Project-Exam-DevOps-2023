28afdd24cf7666b9ba0f03bb83b376fc213f9d5c
bacalu93 <andrei.bacalu93@gmail.com>
2023-07-07 14:13:09 +0200

    update dockerpush.sh

--- first.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# put your domain in this var
-http=http://todo-andrei.link
-
-# save the status in some variable 
-status=`curl $http -k -s -f -o /dev/null && echo "SUCCESS" || echo "ERROR"`    
-
-# print results (or use it in your scripts)
-echo "testing $http=$status"
