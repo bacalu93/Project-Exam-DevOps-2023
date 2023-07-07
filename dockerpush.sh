28c59f1602d9b900a52774e8a9d97ac46c20b223
bacalu93 <115462674+bacalu93@users.noreply.github.com>
2023-07-07 14:14:20 +0200

    Update dockerpush.sh

--- dockerpush.sh
+++ dockerpush.sh
@@ -7,3 +7,12 @@ status=`curl $http -k -s -f -o /dev/null && echo "SUCCESS" || echo "ERROR"`
 
 # print results (or use it in your scripts)
 echo "testing $http=$status"
+if [ $status == "SUCCESS" ]
+then
+    docker tag todoapp_todolist_webapp:latest andrei9310/my-to-do-app-todolist_webapp:latest.
+    docker push andrei9310/my-to-do-app-todolist_webapp:latest.
+    docker tag mysql:8.0.12 andrei9310/mysql-todo-app:latest.
+    docker push andrei9310/mysql-todo-app:latest.
+else
+    echo "This webpage is not available!"
+fi
