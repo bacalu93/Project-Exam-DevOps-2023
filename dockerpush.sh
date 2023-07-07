7729587ef6dbae8b17edc2689b473a2c9177b534
bacalu93 <115462674+bacalu93@users.noreply.github.com>
2023-07-07 14:25:14 +0200

    Update dockerpush.sh

--- dockerpush.sh
+++ dockerpush.sh
@@ -13,6 +13,7 @@ then
     docker push andrei9310/my-to-do-app-todolist_webapp:latest.
     docker tag mysql:8.0.12 andrei9310/mysql-todo-app:latest.
     docker push andrei9310/mysql-todo-app:latest.
+    echo "This webpage is available and the docker's images was pushed to https://hub.docker.com/ !"
 else
     echo "This webpage is not available!"
 fi
