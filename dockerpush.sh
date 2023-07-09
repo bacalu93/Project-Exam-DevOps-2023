575e255192ae7eed3796daeacf8c4ade32744777
bacalu93 <115462674+bacalu93@users.noreply.github.com>
2023-07-09 15:20:52 +0200

    Update dockerpush.sh

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # put your domain in this var
-http=http://todo-andrei.link
+
+MYWEBSITE=http://todo-andrei.link
 
 # save the status in some variable 
-status=`curl $http -k -s -f -o /dev/null && echo "SUCCESS" || echo "ERROR"`    
+STATUS=`curl $MYWEBSITE -k -s -f -o /dev/null && echo "SUCCESS" || echo "ERROR"`    
 
 # print results (or use it in your scripts)
-echo "testing $http=$status"
-if [ $status == "SUCCESS" ]
+echo "testing $MYWEBSITE=$STATUS"
+if [ $STATUS == "SUCCESS" ];
 then
-    docker tag todoapp_todolist_webapp:latest andrei9310/my-to-do-app-todolist_webapp:latest.
-    docker push andrei9310/my-to-do-app-todolist_webapp:latest.
-    docker tag mysql:8.0.12 andrei9310/mysql-todo-app:latest.
-    docker push andrei9310/mysql-todo-app:latest.
+    docker tag todoapp_todolist_webapp:latest andrei9310/my-to-do-app-todolist_webapp:latest
+    docker push andrei9310/my-to-do-app-todolist_webapp:latest
+    docker tag mysql:8.0.12 andrei9310/mysql-todo-app:latest
+    docker push andrei9310/mysql-todo-app:latest
     echo "This webpage is available and the docker's images was pushed to https://hub.docker.com/ !"
 else
     echo "This webpage is not available!"
 fi
+
