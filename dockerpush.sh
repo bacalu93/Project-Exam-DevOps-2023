4499f0ab884387e4b23b638b433ddfc0841b4f4e
bacalu93 <115462674+bacalu93@users.noreply.github.com>
2023-07-15 10:47:47 +0200

    Update dockerpush.sh

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-# put your domain in this var
-
-MYWEBSITE=http://todo-andrei.link
-
-# save the status in some variable 
-STATUS=`curl $MYWEBSITE -k -s -f -o /dev/null && echo "SUCCESS" || echo "ERROR"`    
-
-# print results (or use it in your scripts)
-echo "testing $MYWEBSITE=$STATUS"
+. ./test.sh
 if [ $STATUS == "SUCCESS" ];
 then
     docker tag todoapp_todolist_webapp:latest andrei9310/my-to-do-app-todolist_webapp:latest
